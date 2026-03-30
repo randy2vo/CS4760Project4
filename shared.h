54389f7b1a5773259669f1aba473b465b3cd71b1
Randy <randy2vo@gmail.com>
2026-03-30 13:58:02 -0500

    Moved the struct PCB from worker to shared.h

--- shared.h
+++ shared.h
@@ -3,38 +3,57 @@
 
 #include <sys/types.h>
 
+// ------------------------------
+// Constants
+// ------------------------------
 const int TABLE_SIZE = 20;
-const unsigned int BILLION = 1000000000;
-const unsigned int QUANTUM_NS = 25000000; // 25 ms
+const unsigned int BILLION = 1000000000U;
+const unsigned int QUANTUM_NS = 25000000U;   // 25 ms
+const unsigned int BLOCKED_TIME_NS = 100000000U; // 100 ms
 
+// Worker actions returned to oss
+const int ACTION_FULL_QUANTUM = 0;
+const int ACTION_BLOCKED      = 1;
+const int ACTION_TERMINATED   = 2;
+
+// ------------------------------
+// Shared simulated clock
+// ------------------------------
 struct SimClock {
     unsigned int seconds;
     unsigned int nanoseconds;
 };
 
+// ------------------------------
+// Message queue structure
+// mtype = destination process type
+// ------------------------------
 struct Message {
-    long mtype;
-    int index;       // PCB slot or local pid
-    int quantum;     // time quantum sent by oss
-    int usedTime;    // actual time used by worker
-    int action;      // 0 = full quantum, 1 = blocked, 2 = terminated
+    long mtype;                // required by System V queues
+    int index;                 // PCB slot or simulated pid
+    unsigned int quantum;      // quantum sent by oss
+    unsigned int usedTime;     // time actually used by worker
+    int action;                // full quantum / blocked / terminated
 };
 
+// ------------------------------
+// Process Control Block
+// ------------------------------
 struct PCB {
-    bool occupied;
-    pid_t pid;
-    int localPid;
+    bool occupied;                   // slot in use or not
+    pid_t pid;                       // real Linux pid
+    int localPid;                    // simulated pid for logging
 
-    unsigned int startSeconds;
+    unsigned int startSeconds;       // when created
     unsigned int startNano;
 
-    unsigned int serviceTimeSeconds;
+    unsigned int serviceTimeSeconds; // total CPU time used
     unsigned int serviceTimeNano;
 
-    unsigned int eventWaitSec;
+    unsigned int eventWaitSec;       // when blocked process wakes up
     unsigned int eventWaitNano;
 
-    bool blocked;
+    bool blocked;                    // currently blocked?
 };
 
 #endif
